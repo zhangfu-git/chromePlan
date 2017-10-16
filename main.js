86cc49706c78adf3a780fe4a4941a05463b5cb1f
zhangfu-git <zhangfu@nasetech.com>
2017-10-16 21:11:00 +0800

    init

---
+++ main.js
@@ -0,0 +1,313 @@
+var aImg = [
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507975848076&di=0cd8142a010e483bf0da946d8d6fa374&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D2293279151%2C2759833220%26fm%3D214%26gp%3D0.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507975844575&di=ff1f0341d798d3a943301460b1aecce3&imgtype=0&src=http%3A%2F%2Ffile.mumayi.com%2Fforum%2F201505%2F05%2F172929a4gkwtb4jwyjbruw.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507975985668&di=a7c01aad61ed3f9d4dbdc30a936f2797&imgtype=0&src=http%3A%2F%2Fimg4.web07.cn%2FUPics%2FBizhi%2F2016%2F1110%2F197504101341261.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976021282&di=e8f2a46034f8550cca9ccdfd5b976592&imgtype=0&src=http%3A%2F%2Fp4.gexing.com%2Fshaitu%2F20130429%2F2140%2F517e784856815.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976059046&di=0368c617e7db393dbca2bd23c0ed37a5&imgtype=0&src=http%3A%2F%2Fimg3.iqilu.com%2Fdata%2Fattachment%2Fforum%2F201308%2F22%2F162837cd7yd0yi5999lkmg.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976089514&di=bc461e2a1553243953eb55999c9b1281&imgtype=0&src=http%3A%2F%2Fp2.wmpic.me%2Farticle%2F2014%2F07%2F04%2F1404461911_ZRLbtPKq.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976126102&di=56aad0d51d30b5ff5a219500663b6726&imgtype=0&src=http%3A%2F%2Fwww.bz55.com%2Fuploads%2Fallimg%2F120818%2F1-120QPZ331.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976126090&di=d5dd536ac93ccce0fe61f644028aa830&imgtype=0&src=http%3A%2F%2F4493bz.1985t.com%2Fuploads%2Fallimg%2F150317%2F4-15031GA038.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976177724&di=c9ce156fb76553c4205673398133d852&imgtype=jpg&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D3105041861%2C1691488416%26fm%3D214%26gp%3D0.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976126062&di=bd4275751588deeaede011129e852d23&imgtype=0&src=http%3A%2F%2F4493bz.1985t.com%2Fuploads%2Fallimg%2F150306%2F4-150306150Z5.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976126056&di=823aea72cbb1ed18629816c9925ba05e&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2F201612%2F20%2Fefa51f6fcf654c6c3aa31bab2b66eca2.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976126044&di=c548d660a5bf994f50ae7fff75248f5f&imgtype=0&src=http%3A%2F%2Fwww.pp3.cn%2Fuploads%2F201504%2F2015050506.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976246974&di=639ab817acfde3ca434da18c13fb53e5&imgtype=0&src=http%3A%2F%2Fwww.pp3.cn%2Fuploads%2F201406%2F1403577584281.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976246968&di=a02190f1513e26cb1f09a9df5c2140f5&imgtype=0&src=http%3A%2F%2Fwww.rmzt.com%2Fuploads%2Fallimg%2F150918%2F1-15091Q04548.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976246968&di=cd6b21fed875eee8627837bf1aed6bb9&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F1%2F57bd443597194.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976246967&di=e0239a543ec65815cfdd53c113fcd85f&imgtype=0&src=http%3A%2F%2Fimg3.xiazaizhijia.com%2Fwalls%2F20160714%2F1440x900_447c2c4cc7f6991.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976315117&di=7314dcacab0a09b1769fb6141e78303c&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2015%2Fgha%2F11%2F2003%2F08.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976315115&di=fbe1aa0682b3447eb8b72a9336418ffc&imgtype=0&src=http%3A%2F%2Fwww.pp3.cn%2Fuploads%2F201407%2F3698745123.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976416991&di=085754c26fb1a7f00c2b3cdd3bc385d9&imgtype=0&src=http%3A%2F%2F4493bz.1985t.com%2Fuploads%2Fallimg%2F140924%2F3-140924103356.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976416989&di=f97361fa386997563cd8615388a2efb8&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2Fd%2Ffile%2F201702%2F23%2F9fb4064b0d901cebfe1a7c9f7f2f8f56.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976416988&di=97be3ed693fae903b03c64f1d00ede7a&imgtype=0&src=http%3A%2F%2Fwww.pp3.cn%2Fuploads%2F201408%2F201408070848.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976416983&di=281e73f144c2fc9c70999b2556084710&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F9%2F526903c0223dc.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976574504&di=6be4fef47f2d3ae3f656207233ed0241&imgtype=jpg&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D2452611840%2C1462755167%26fm%3D214%26gp%3D0.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976609381&di=606c8b99374a568ff3f838908881b814&imgtype=0&src=http%3A%2F%2Fwww.bz55.com%2Fuploads%2Fallimg%2F150513%2F139-150513091934.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976716829&di=a259580b9cc5ce9d6e61af85d5304d07&imgtype=jpg&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D3389194781%2C965779959%26fm%3D214%26gp%3D0.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1508053447&di=1d24bdd394f26c17a2c460d63e6309b2&src=http://imgstore.cdn.sogou.com/app/a/100540002/633557.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976833476&di=1c3c0fb0002d6cb795667a12071acca8&imgtype=0&src=http%3A%2F%2Fimgstore.cdn.sogou.com%2Fapp%2Fa%2F100540002%2F713182.jpg',
+  'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3081583342,3081137729&fm=27&gp=0.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976797706&di=b94713906b11e93052e082a53346f84e&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2Fb%2F5440b93364a55.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507976759152&di=1edeafb29703230cb0c750118a2784ba&imgtype=0&src=http%3A%2F%2Fimg17.3lian.com%2F201612%2F19%2Fd00b021b41cbf40afc6aa4960d4250c7.jpg'
+];
+
+var dateMange = {
+  getTime: function(date) {
+    var type = Object.prototype.toString.call(date);
+    console.log(type, '@@@@@@@@@@@@@@@@@', date)
+    if (type === '[object Undefined]') {
+      date = new Date();
+    } else {
+      date = new Date(date);
+    }
+    return this.format(date).time;
+  },
+  getYearMonth: function (date) {
+    var type = Object.prototype.toString.call(date);
+    if (type !== '[object Date]') {
+      date = new Date();
+    }
+    return this.format(date.time);
+  },
+  getWeek: function(date) {
+    var aWeek = ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+    var type = Object.prototype.toString.call(date);
+    if (type !== '[object Date]') {
+      date = new Date();
+    }
+    return aWeek[date.getDay()];
+  },
+  format: function (date) {
+    var year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate(),
+        hours = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds();
+
+    seconds <= 9 ? (seconds = '0' + seconds) : seconds;
+    minutes <= 9 ? (minutes = '0' + minutes) : minutes;
+    hours <= 9 ? (hours = '0' + hours) : hours;
+    return {
+      date: year + '年'　+ month + '月' + day + '日',
+      time: hours + ':' + minutes + ':' + seconds
+    }
+  },
+  getDesc: function() {
+    var hours = new Date().getHours();
+    if (hours >= 0 && hours <= 5) {
+      return '凌晨';
+    } else if (hours > 5 && hours <= 11) {
+      return '早上';
+    } else if (hours > 11 && hours <= 13) {
+      return '中午';
+    } else if (hours > 13 && hours < 17) {
+      return '下午';
+    }　else if (hours >= 17 && hours <= 19) {
+      return '傍晚';
+    } else if (hours > 19 && hours <= 23) {
+      return '晚上';
+    }
+  }
+}
+
+var UIControll = {
+  init: function () {
+    var me = this;
+    window.onload = function() {
+      me.randomBagPic();
+      me.setTimeUI();
+
+      setInterval(function() {
+        me.setTimeUI.call(me)
+      }, 1000);
+
+      me.onNextBtnClick();
+      me.onPlanCloseBtn();
+      me.onAddPlan();
+      me.onSeePlanList();
+      me.onCloseBtnPlan();
+      me.onAddName();
+
+      me.checkedHasName();
+    }
+  },
+  checkedHasName: function() {
+    var name = dataMange.getData('name');
+    if (name) {
+      this.hideAddNameModule();
+    }
+  },
+  onAddName: function() {
+    var me = this;
+    document.querySelector('.saveNameBtn').onclick = function() {
+      var name = document.getElementById('nameInput').value;
+      dataMange.addData('name', name);
+      me.hideAddNameModule();
+    }
+  },
+  hideAddNameModule: function() {
+    document.querySelector('.mask').classList.add('hide');
+    document.querySelector('.save_name_module').classList.add('hide');
+  },
+  createdPlanList: function(data) {
+    var result = '',
+        end,
+        start;
+    data && data.forEach(function(item, index) {
+      start = dateMange.format(new Date(item.start));
+      end = dateMange.format(new Date(item.end));
+      console.log(start)
+      result += '<div class="plan_block"><div class="start">开始时间： ' + start.date + start.time + '</div><div class="end">' + '结束时间： ' + end.date + end.time + '</div><div class="plan">' +
+        item.plan + '</div></div>'
+    });
+    if (!result) {
+      result = '您当前还没有计划安排'
+    }
+    document.querySelector('.all_plan_list').innerHTML = result;
+  },
+  onCloseBtnPlan: function() {
+    var me = this;
+    document.querySelector('.close_btn_plan').onclick = function() {
+      me.hidePlanList();
+    }
+  },
+  onSeePlanList: function() {
+    var me = this;
+    document.querySelector('.selectPlanBtn').onclick = function() {
+      var planList = dataMange.getData('plan');
+      me.createdPlanList(planList);
+      me.showPlanList();
+    }
+  },
+  showPlanList: function() {
+    document.querySelector('.mask').classList.remove('hide');
+    document.querySelector('.all_plan').classList.add('active');
+  },
+  hidePlanList: function() {
+    document.querySelector('.mask').classList.add('hide');
+    document.querySelector('.all_plan').classList.remove('active');
+  },
+  onAddPlan: function() {
+    var me = this;
+    document.querySelector('.add_plan_btn').onclick = function() {
+      var plan = me.getPlanContent(),
+          tips = document.querySelector('.add_plan_tips'),
+          allPlans = null;
+      console.log(plan, '@@@@@@@@@@@@@')
+      if (!plan.plan) {
+        tips.innerText = '计划不能为空！';
+      } else if (!plan.start) {
+        tips.innerText = '计划开始日期不能为空！';
+      } else if (!plan.end) {
+        tips.innerText = '计划结束日期不能为空！';
+      } else {
+        tips.innerText = '';
+        allPlans = dataMange.getData('plan');
+
+        if (!allPlans) {
+          allPlans = [{
+            start: plan.start,
+            end: plan.end,
+            plan: plan.plan
+          }]
+        } else {
+          allPlans.push({
+            start: plan.start,
+            end: plan.end,
+            plan: plan.plan
+          });
+        }
+
+        dataMange.addData('plan', allPlans);
+        me.hidePlanModule();
+      }
+    }
+  },
+  getPlanContent: function() {
+    var start = document.querySelector('.plan_start_date').value,
+        end = document.querySelector('.plan_end_date').value;
+    return {
+      'start': start,
+      'end': end,
+      'plan': document.querySelector('.plan_content').value
+    }
+  },
+  getCurrentPlan: function(key) {
+    var allPlans = dataMange.getData('plan') || [],
+        currentPlan = null,
+        currentTime = Date.now(),
+        start,
+        end;
+    allPlans.forEach(function(item, index) {
+      start = new Date(item.start).getTime();
+      end = new Date(item.end).getTime();
+      if (currentTime >= start && currentTime <= end) {
+        currentPlan = item.plan;
+        return false;
+      }
+    });
+    if (currentPlan) {
+      this.getPlanDOM().innerText = currentPlan;
+    } else {
+      this.getPlanDOM().innerText = '';
+    }
+  },
+  getPlanDOM: function() {
+    if (this.getPlanDOM.cache) return this.getPlanDOM.cache;
+    return this.getPlanDOM.cache = document.querySelector('.plan_p');
+  },
+  onPlanCloseBtn: function () {
+    var me = this;
+    document.querySelector('.close_btn').onclick = function() {
+      me.hidePlanModule();
+    }
+  },
+  onNextBtnClick: function() {
+    var me = this;
+    document.querySelector('.addPlanBtn').onclick = function() {
+      me.showPlanModule();
+    }
+  },
+  hidePlanModule: function() {
+    document.querySelector('.task_module').classList.remove('active');
+    document.querySelector('.mask').classList.add('hide');
+  },
+  showPlanModule: function() {
+    document.querySelector('.task_module').classList.add('active');
+    document.querySelector('.mask').classList.remove('hide');
+  },
+  randomBagPic: function() {
+    var index = Math.floor(Math.random() * 31),
+        imgSrc = aImg[index];
+    console.log(imgSrc)
+    document.body.style.backgroundImage = 'url(' + imgSrc + ')';
+  },
+  getTimeBox: function () {
+    if (this.getTimeBox.cache) return this.getTimeBox.cache;
+    return this.getTimeBox.cache = document.getElementById('timeBox');
+  },
+  setTimeUI: function () {
+    var currTime = dateMange.getTime(),
+        timeTips = dateMange.getWeek() + ' ' + currTime;
+    this.getTimeBox().innerText = timeTips;
+    this.setGreetins();
+    this.getCurrentPlan();
+  },
+  setGreetins: function () {
+    var greetings = document.querySelector('.greetins');
+    var timeDesc = dateMange.getDesc();
+    var name = dataMange.getData('name');
+    if (name === null) {
+      greetings.innerText = '我还不知道怎么称呼您？';
+    } else {
+      greetings.innerText = name + ', ' + timeDesc + '好！';
+    }
+  }
+}
+
+var dataMange = {
+  addData: function(key, value) {
+    if (typeof value === 'object') {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      window.localStorage.setItem(key, value);
+    }
+  },
+  getData: function(key) {
+    var data = window.localStorage.getItem(key),
+        result = null;
+    try {
+      result = JSON.parse(data);
+    } catch(e) {
+      result = data;
+    }
+    return result;
+  },
+  deleteData: function(key) {
+    window.localStorage.removeItem(key);
+  },
+
+}
+
+UIControll.init();
